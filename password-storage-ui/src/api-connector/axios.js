fa02624de8d3a298639f6af189f5cf114b2acd6a
Geraldi <geraldi@coindoui-MacBookPro.local>
2020-01-16 12:13:01 +0700

    ui initial set up

---
+++ password-storage-ui/src/api-connector/axios.js
@@ -0,0 +1,66 @@
+import axios from "axios"
+
+const singleton = Symbol()
+const singletonEnforcer = Symbol()
+
+class Axios {
+	constructor(enforcer) {
+		if (enforcer !== singletonEnforcer) {
+			throw new Error("Cannot construct singleton")
+		}
+		this.session = axios.create({
+			headers: {
+				post: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			},
+			timeout: 30000,
+			baseURL: `http://${window.location.hostname}:1616`
+		})
+		this._interceptor = null
+		this.token = ""
+
+		// this._proxyRequest()
+	}
+
+	static get instance() {
+		if (!this[singleton]) {
+			this[singleton] = new Axios(singletonEnforcer)
+		}
+		return this[singleton]
+	}
+
+	get = (...params) => this.session.get(...params)
+	post = (...params) => this.session.post(...params)
+	put = (...params) => this.session.put(...params)
+	delete = (...params) => this.session.delete(...params)
+	patch = (...params) => this.session.patch(...params)
+}
+
+/**
+ * Reduce response to get only the data
+ * @param {object} res - response object
+ */
+export const axiosMiddleware = res => {
+	if (!res.data) return res
+	return res.data.data
+}
+
+/**
+ * Handle error from connection
+ * @param {object} err - error object
+ */
+export const axiosErrorHandler = err => {
+	if (!err.response)
+		return Promise.reject({
+			data: "connectionRefused",
+			message: "connectionRefused",
+		})
+	const { data } = err.response
+	if (!data) return Promise.reject(err)
+	if (!data.data && data.message) data.data = data.message
+	if (!data.message && data.data) data.message = data.data
+	return Promise.reject(data)
+}
+
+export default Axios.instance
---
+++ password-storage-ui/src/store/index.js
@@ -0,0 +1,50 @@
+import createBrowserHistory from "history/createBrowserHistory"
+import { applyMiddleware, compose, createStore } from "redux"
+import { connectRouter, routerMiddleware } from "connected-react-router/immutable"
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"
+import penderMiddleware from "redux-pender"
+import { Map } from "immutable"
+
+import rootReducer from "./reducers"
+
+const history = createBrowserHistory()
+
+const composeEnhancers =
+	process.env.NODE_ENV === "production"
+		? compose
+		: composeWithDevTools({
+				actionsBlacklist: [
+					"@@redux-pender/SUCCESS",
+					"@@redux-pender/FAILURE",
+					"@@redux-pender/PENDING",
+				],
+				maxAge: 1000,
+		  })
+
+function configureStore(history) {
+	//Init middlewares
+	const middlewares = [routerMiddleware(history), penderMiddleware()];
+  
+	//Init enhancer
+	const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+  
+	//Store creation
+	const store = createStore(
+	  rootReducer(history),
+	  enhancer
+	);
+  
+	return store;
+ }
+
+ const store = configureStore(history)
+
+ if (rootReducer.hot) {
+	rootReducer.hot.accept("./reducers", () => {
+		const nextRootReducer = require("./reducers").default
+		store.replaceReducer(connectRouter(history)(nextRootReducer))
+	})
+}
+
+export { history }
+export default store
